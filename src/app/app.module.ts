077a75bee211b3ee26f6cd031f8af297e8353299
guptsujit <guptsujit@gmail.com>
2017-10-18 19:59:25 +0530

    Created routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeMenuComponent } from './home-menu/home-menu.component';
-import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes} from '@angular/router';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { NewsComponent } from './news/news.component';
-import { ContactusFormComponent } from './contactus-form/contactus-form.component';
 import { FormsModule } from '@angular/forms';
-import { UserlistComponent } from './userlist/userlist.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { Ng2OrderModule } from 'ng2-order-pipe';
-import {PaginationDirective} from 'angular2-bootstrap-pagination/directives/pagination.directive';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { UsercountComponent } from './usercount/usercount.component';
-import { ProductComponent } from './product/product.component';
-import { UsertitlePipe } from './userlist/usertitle.pipe';
-import { LoginComponent } from './login/login.component';
+import { RoutingModule,routingComponents } from './routing.module';
+import { AppComponent } from './app.component';
 
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent, data: {title: 'Home'} },
-  { path: 'login', component: LoginComponent , data: {title: 'Login'}},
-  { path: 'aboutus', component: AboutusComponent, data: {title: 'Aboutus'} },
-  { path: 'contactus',   component: ContactusFormComponent ,data: {title: 'Contactus'}},
-  { path: 'signup', component: ReactiveFormComponent,data: {title: 'Signup'} },
-  { path: 'contactus/:userid',   component: ContactusFormComponent,data: {title: 'Contactus Update'} },
-  { path: 'userlist', component: UserlistComponent,data: {title: 'User list'} },
-  { path: 'product', component: ProductComponent, data: {title: 'Product'}},
-
-  { path: '**', redirectTo: 'pagenotfound' },
-  { path: '**',  component: PageNotFoundComponent }
-];
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeMenuComponent,
-    HomeComponent,
-    AboutusComponent,
-    NewsComponent,
-    ContactusFormComponent,
-    UserlistComponent,
-    PageNotFoundComponent,
-    PaginationDirective,
-    ReactiveFormComponent,
-    UsercountComponent,
-    ProductComponent,
-    UsertitlePipe,
-    LoginComponent,
+    AppComponent,routingComponents
   ],
   imports: [
-    BrowserModule,FormsModule,HttpModule,RouterModule.forRoot(appRoutes)
-    ,Ng2SearchPipeModule,Ng2OrderModule,ReactiveFormsModule
+    BrowserModule,FormsModule,HttpModule,Ng2SearchPipeModule,Ng2OrderModule,ReactiveFormsModule,RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users/users.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { NewsComponent } from './news/news.component';
+import { ContactusFormComponent } from './contactus-form/contactus-form.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PaginationDirective } from 'angular2-bootstrap-pagination/directives/pagination.directive';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { UsercountComponent } from './usercount/usercount.component';
+import { ProductComponent } from './product/product.component';
+import { UsertitlePipe } from './userlist/usertitle.pipe';
+import { LoginComponent } from './login/login.component';
+import { LeftmenuComponent } from './leftmenu/leftmenu.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, data: { title: 'Home' } },
+  { path: 'login', component: LoginComponent, data: { title: 'Login' } },
+  { path: 'aboutus', component: AboutusComponent, data: { title: 'Aboutus' }, children: [{ path: 'aboutus', component: LeftmenuComponent }] },
+  { path: 'contactus', component: ContactusFormComponent, data: { title: 'Contactus' } },
+  { path: 'signup', component: ReactiveFormComponent, data: { title: 'Signup' } },
+  { path: 'contactus/:userid', component: ContactusFormComponent, data: { title: 'Contactus Update' } },
+  { path: 'userlist', component: UserlistComponent, data: { title: 'User list' } },
+  { path: 'product', component: ProductComponent, data: { title: 'Product' } },
+  { path: '**', redirectTo: 'pagenotfound' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forRoot(appRoutes)
+  ], exports: [RouterModule],
+  declarations: []
+})
+export class RoutingModule { }
+export const routingComponents = [
+  UsersComponent, HeaderComponent, FooterComponent, HomeMenuComponent, HomeComponent, AboutusComponent, NewsComponent,
+  ContactusFormComponent, UserlistComponent, PageNotFoundComponent, UsercountComponent, ProductComponent,
+  UsertitlePipe, LoginComponent, LeftmenuComponent, ReactiveFormComponent
+
+];
